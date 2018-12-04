c485a37965298c49611fe70510dcdcbb0637ca37
tkyaaida <tkyaaida@gmail.com>
2018-12-04 18:02:31 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     name='gym_trainer',
     packages=find_packages('gym_trainer'),
     author='Takuya Aida',
+    version='0.1.0',
     install_requires=[
-        'gym', 'matplotlib', 'torch',
+        'gym==0.10.9', 'matplotlib==3.0.2', 'chainer==5.1.0',
     ]
 )
