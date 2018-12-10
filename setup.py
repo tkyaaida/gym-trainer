128aac153c6f16b64dec87c3b52d7d024ec290f5
tkyaaida <tkyaaida@gmail.com>
2018-12-10 23:27:01 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author='Takuya Aida',
     version='0.1.0',
     install_requires=[
-        'gym==0.10.9', 'matplotlib==3.0.2', 'chainer==5.1.0',
+        'gym==0.10.9', 'matplotlib==3.0.2', 'chainer==5.1.0', 'cupy==5.1.0', 'torch==1.0.0', 'torchvision==0.2.1',
+        'optuna==0.4.0'
     ]
 )
